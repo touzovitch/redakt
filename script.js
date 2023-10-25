893169e10b8aafe4f5288e992b83a8bc7574bbe6
touzovitch <144453396+touzovitch@users.noreply.github.com>
2023-10-25 17:10:41 +0200

    Change JS click event logic
    To solve permission conflict with short.io

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const key = "i<3redakt-06644fe7-6503-484b-9ae0-02db2dda7466";
 
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('buttonEncrypt').addEventListener('click', encrypt);
+});
+
 function encrypt() {
   const inputText = document.getElementById('inputText').value;
   const encryptedText = CryptoJS.AES.encrypt(inputText, key).toString();
@@ -8,6 +12,10 @@ function encrypt() {
   document.getElementById("info-text").style.display = "none";
 };
 
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('buttonDecrypt').addEventListener('click', decrypt);
+});
+
 function decrypt() {
   const inputText = document.getElementById('inputText').value.replace('redakt.org-','').replace('redakt-','').replace('&&&','');
   const decryptedText = CryptoJS.AES.decrypt(inputText, key).toString(CryptoJS.enc.Utf8);
@@ -15,6 +23,10 @@ function decrypt() {
   document.getElementById("copied").style.display = "none";
   document.getElementById("info-text").style.display = "block";
 };
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('outputText').addEventListener('click', copyText);
+});
 
 function copyText() {
   const textToCopy = document.getElementById("outputText").innerText;
